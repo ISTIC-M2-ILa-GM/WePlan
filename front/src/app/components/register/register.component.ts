ff8f5f6eb0bec27db0f8a991a8ea6deb46c50a7f
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-10-14 20:54:33 +0200

    added register component #29

--- front/src/app/components/register/register.component.ts
+++ front/src/app/components/register/register.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+  repeat = '';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit = () => {
   }
 
+  onSubmit = () => {
+    console.log(`First name: ${this.firstName} Last name: ${this.lastName}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`Password: ${this.password} Repeat: ${this.repeat}`)
+  }
 }
