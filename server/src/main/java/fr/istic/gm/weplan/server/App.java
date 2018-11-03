d476e3b7d9b623c04aa89dd5ff676c1f12b7b6d8
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-11-03 01:57:15 +0100

    enabled scheduling, started experimenting scheduled function using cron-like parameter #9

--- server/src/main/java/fr/istic/gm/weplan/server/App.java
+++ server/src/main/java/fr/istic/gm/weplan/server/App.java
@@ -1,13 +1,19 @@
 package fr.istic.gm.weplan.server;
 
+import fr.istic.gm.weplan.infra.client.weather.impl.WeatherClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * The application
  */
 @SpringBootApplication
+@EnableScheduling
 public class App {
+
+    private WeatherClientImpl weatherClient;
 
     /**
      * The main application.
@@ -16,5 +22,10 @@ public class App {
      */
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void doTheHarlemShake(){
+        System.out.println("Hey there ! Time to go to sleep now !");
     }
 }
